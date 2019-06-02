05d49a1cfefdaf69c0cb8d396b6a63236a03387a
Abhinaw Gupta <abhinaw.sg@gmail.com>
2019-06-02 18:36:40 +0530

    Update db.js

--- src/helper/db.js
+++ src/helper/db.js
@@ -3,7 +3,7 @@ const config = require('../config/config');
 const mongoose = require('mongoose');
 const user = require('../model/user');
 
-if(mongoose.connect(process.env.MONGODB_URI || config.mongodbUrl.url + config.mongodbUrl.dbName, { useCreateIndex: true, useNewUrlParser: true }))
+if(mongoose.connect(process.env.MONGODB_URI || config.mongodbUrl.url + config.mongodbUrl.dbName, { useCreateIndex: true, useNewUrlParser: true })
     {
         console.log('Database Connected Successfully')
     }
